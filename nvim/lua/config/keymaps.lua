cd7f23a3b29a51068a8a38586bbbd2ce508eaa30
McLOVIN <shortcut7777@gmail.com>
2024-06-12 12:54:46 +0800

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jj", "<esc>")
+local map = vim.keymap.set
+map("i", "jj", "<esc>")
+map("i", "<c-p>", "<up>")
+map("i", "<c-n>", "<down>")
+map("i", "<c-b>", "<left>")
+map("i", "<c-f>", "<right>")
+map("i", "<c-a>", "<Esc>I")
+map("i", "<c-e>", "<end>")
+map("i", "<c-d>", "<delete>")
+map("i", "<c-h>", "<bs>")
