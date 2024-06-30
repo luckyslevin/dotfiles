486038cec9019c72c66896d7e4cd3d27cba50fbd
shortcut <shortcut@shortcuts-MacBook-Pro.local>
2024-06-30 21:04:35 +0800

    Update nvim

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,30 +1,20 @@
 return {
   "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-emoji",
-  },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local cmp = require("cmp")
 
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<C-N>"] = cmp.config.disable,
       ["<C-P>"] = cmp.config.disable,
+      ["<C-N>"] = cmp.config.disable,
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          cmp.select_next_item()
-        elseif vim.snippet.active({ direction = 1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(1)
-          end)
-        elseif has_words_before() then
-          cmp.complete()
+          local entries = cmp.get_entries()
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+
+          if #entries == 1 then
+            cmp.confirm()
+          end
         else
           fallback()
         end
